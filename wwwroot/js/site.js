36990be1b97f813ba944414863b3b254aebff12a
AE-projects <Matt.E.Robinson@outlook.com>
2024-04-08 21:24:28 +0100

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+    function scrollToSection(sectionId) {
+            var section = document.getElementById(sectionId);
+    section.classList.add('highlight');
+    setTimeout(function () {
+        section.classList.remove('highlight');
+            }, 3000); // Timer
+    section.scrollIntoView({behavior: 'smooth' });
+        }
